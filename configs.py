7365a94255070a46ed30172a6deb0a9e272419e6
Khalbal <99757564+Khalbal@users.noreply.github.com>
2024-02-08 15:08:43 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -30,7 +30,7 @@ else:
     URL = "http://{}/".format(FQDN)
 UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
 BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001987654567")).split())) 
-STREAM_LOGS = environ.get('STREAM_LOGS','-1002058704379')
+STREAM_LOGS = environ.get('STREAM_LOGS','-1002053605304')
 SESSION = environ.get('SESSION','MissRozy')
 CUSTOM_CAPTION = environ.get('CUSTOM_CAPTION')
 
@@ -44,7 +44,7 @@ class Config(object):
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1859414374"))
 	DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://khalbal:pankaj00@cluster0.cungjwq.mongodb.net/?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001824836547")
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1002095937652")
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1002053605304")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
